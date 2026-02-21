27236017f1ba964da550e0a7190368c05eb51fa6
divinetechnologies8-cmd <divinetechnologies8@gmail.com>
2026-02-21 02:24:11 -0500

    d

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const app  = express();
+const app = express();
 const cors = require("cors");
 const userRoutes = require("./src/user/routes");
 const categoryRoutes = require("./src/category/routes")
 const sequelize = require("./src/db/db");
 
-app.use(cors({origin:"*"}));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 
-sequelize.sync({alter:false});
-app.use("/api",userRoutes);
-app.use("/api",categoryRoutes);
+sequelize.sync({ force:false });
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
-app.listen(8888,()=>{
+app.listen(8888, () => {
     console.log("server is running on 8888")
 })
--- src/user/controller.js
+++ src/user/controller.js
@@ -1,76 +1,104 @@
 const User = require("./modal");
+const bcrypt = require("bcryptjs");
+const store = async (req, res) => {
+    try {
 
-const store = async(req ,res) =>{
-try {
-    
-const {user_name,user_email,user_password,user_address} = req.body;
-const newUser = await User.create({
-    user_name:user_name,
-    user_email:user_email,
-    user_password:user_password,
-    user_address:user_address
-});
-if (newUser) {
-    res.status(200).json({message:"User Created SuccessFully"})
-}else {
-    res.status(400).json({message:"Failed To Create User"})
-}
-} catch (error) {
-    console.log(error);
-    
-}
+        const { user_name, user_email, user_password, user_address } = req.body;
+
+        const hashePassword = await bcrypt.hash(user_password, 10);
+        console.log("hello ");
+        console.log(hashePassword)
+        const newUser = await User.create({
+            user_name: user_name,
+            user_email: user_email,
+            user_password: user_password,
+            user_address: user_address
+        });
+        // if (newUser) {
+        //     res.status(200).json({message:"User Created SuccessFully"})
+        // }else {
+        //     res.status(400).json({message:"Failed To Create User"})
+        // }
+    } catch (error) {
+        console.log(error);
+
+    }
 }
 
 
-const list = async(req ,res ) =>{
+const login = async (req, res) => {
+    try {
+
+        const { user_email, user_password } = req.body;
+
+        const user = await User.findOne({ where: { user_email: user_email } });
+        if (!user){
+            return res.json({message:"User No Found"})
+        }
+
+    const ismatch = await bcrypt.compare(user_password,user.user_password);
+
+    if (!ismatch){
+        return res.json({message:"password incorrect "})
+    }
+
+res.json({user:user})
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+const list = async (req, res) => {
     try {
         console.log("hello from client");
         const users = await User.findAll();
         res.json(users);
     } catch (error) {
         console.log(error);
-        
+
     }
 }
 
 
-const updatedd = async(req ,res) =>{
+const updatedd = async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
 
-        const {user_name,user_email,user_password, user_address} = req.body;
-const user = await User.findByPk(id);
-if (!user){
-    return res.status(404).json({message:"User Not found"})
-}
+        const { user_name, user_email, user_password, user_address } = req.body;
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(404).json({ message: "User Not found" })
+        }
 
-await user.update({
-user_name : user_name ? user_name :user.user_name,
-user_email:user_email ? user_email :user.user_email,
-user_address: user_address ? user_address : user.user_address
-})
+        await user.update({
+            user_name: user_name ? user_name : user.user_name,
+            user_email: user_email ? user_email : user.user_email,
+            user_address: user_address ? user_address : user.user_address
+        })
 
-res.json({message:"User Updated Successfully "})
+        res.json({ message: "User Updated Successfully " })
 
     } catch (error) {
         console.log(error);
-        
+
     }
 }
 
-const deletedd = async(req ,res ) =>{
+const deletedd = async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const user = await User.findByPk(id);
         if (!user) {
-        return res.json({message:"user Not found"})
+            return res.json({ message: "user Not found" })
         }
 
         await user.destroy();
-        res.json({message:"User Deleted SuccessFully"})
+        res.json({ message: "User Deleted SuccessFully" })
     } catch (error) {
         console.log(error);
-        
+
     }
 }
 
@@ -78,5 +106,6 @@ module.exports = {
     store,
     list,
     updatedd,
-    deletedd
+    deletedd,
+    login
 }
